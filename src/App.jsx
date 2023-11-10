ed22bce84d92633e05c2033cf418913e199785d4
Home <aishamirza0002@gmail.com>
2023-11-10 10:26:33 +0530

    changed router to hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,43 @@
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 
-import { BrowserRouter, Routes , Route, Navigate } from "react-router-dom"
-
-import Product from "./pages/Product"
-import HomePage from "./pages/Home"
-import Pricing from "./pages/Pricing"
-import PageNotFound from "./pages/PageNotFound"
-import Login from "./pages/Login"
-import AppLayout from "./pages/AppLayout"
-import CityList from "./components/CityList"
-import CountryList from "./components/CountryList"
-import City from "./components/City"
-import Form from "./components/Form"
-import { CitiesProvider } from "./contexts/CityProvider"
-import { AuthProvider } from "./contexts/AuthContext"
-  /* eslint-disable react/prop-types */
-
+import Product from "./pages/Product";
+import HomePage from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
+import { CitiesProvider } from "./contexts/CityProvider";
+import { AuthProvider } from "./contexts/AuthContext";
+/* eslint-disable react/prop-types */
 
 function App() {
- 
   return (
-  <AuthProvider>
-  <CitiesProvider>
-  <BrowserRouter>
-  <Routes>
-    <Route index element={<HomePage />} />
-    <Route path="pricing" element={<Pricing />} />
-    <Route path="product" element={<Product />} />
-    <Route path="login" element={<Login />} />
-    <Route path="app" element={<AppLayout />}>
-      <Route index  element={<Navigate replace to="cities"/>}/>
-      
-      <Route path="cities" element={<CityList  />}/>
-      <Route path="cities/:id" element={<City />}/>
-      <Route path="countries" element={<CountryList/>}/>
-      <Route path="form" element={<Form />}/>
-      
-    </Route>
-    <Route path="*" element={<PageNotFound />} />
-  </Routes>
-  </BrowserRouter>
-  </CitiesProvider>
-  </AuthProvider>
-)}
+    <AuthProvider>
+      <CitiesProvider>
+        <HashRouter>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="product" element={<Product />} />
+            <Route path="login" element={<Login />} />
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<Navigate replace to="cities" />} />
 
-export default App
+              <Route path="cities" element={<CityList />} />
+              <Route path="cities/:id" element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </HashRouter>
+      </CitiesProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
